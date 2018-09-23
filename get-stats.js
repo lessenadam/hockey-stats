19172ae918bd7a7755f09201fd2026970407f747
Adam Lessen <lessena@gmail.com>
2018-09-22 19:52:48 -0700

    Trim text and add script

--- get-stats.js
+++ get-stats.js
@@ -11,21 +11,28 @@ async function run({ month = 3, day = 6, year = 2018 } = {}) {
     throw err;
   }
 
-  const data = await page.evaluate(() => {
-    const games = Array.from(document.querySelectorAll('.game_summary'));
-    const HOME_TEAM = '.teams tbody tr:nth-of-type(1) td:nth-of-type(1) a';
-    const HOME_SCORE = '.teams tbody tr:nth-of-type(1) td:nth-of-type(2)';
-    const AWAY_TEAM = '.teams tbody tr:nth-of-type(2) td:nth-of-type(1) a';
-    const AWAY_SCORE = '.teams tbody tr:nth-of-type(2) td:nth-of-type(2)';
-    return games.map((game) => ({
-      homeTeam: game.querySelector(HOME_TEAM).innerText,
-      homeScore: game.querySelector(HOME_SCORE).innerText,
-      awayTeam: game.querySelector(AWAY_TEAM).innerText,
-      awayScore: game.querySelector(AWAY_SCORE).innerText,
-    }));
-  });
-
-  console.log(data);
+  //
+  // if the url redirects, it means there weren't any games for that day
+  //
+  console.log(page.url());
+  if (page.url() === url) {
+    const data = await page.evaluate(() => {
+      const games = Array.from(document.querySelectorAll('.game_summary'));
+      const HOME_TEAM = '.teams tbody tr:nth-of-type(1) td:nth-of-type(1) a';
+      const HOME_SCORE = '.teams tbody tr:nth-of-type(1) td:nth-of-type(2)';
+      const AWAY_TEAM = '.teams tbody tr:nth-of-type(2) td:nth-of-type(1) a';
+      const AWAY_SCORE = '.teams tbody tr:nth-of-type(2) td:nth-of-type(2)';
+      return games.map((game) => ({
+        homeTeam: game.querySelector(HOME_TEAM).innerText.trim(),
+        homeScore: game.querySelector(HOME_SCORE).innerText.trim(),
+        awayTeam: game.querySelector(AWAY_TEAM).innerText.trim(),
+        awayScore: game.querySelector(AWAY_SCORE).innerText.trim(),
+      }));
+    });
+    console.log(data);
+  } else {
+    console.log('no games for date', { month, day, year });
+  }
 
   await browser.close();
 }
