b419717ed04e281b79f920941042ab05c68f5d3d
Adam Lessen <lessena@gmail.com>
2019-11-02 22:36:49 -0700

    Module exports in constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,5 @@
-/* eslint-disable import/prefer-default-export */
-export const SCORES_COLLECTION = 'scores-2019';
-export const SCHEDULE_COLLECTION = 'schedule';
-export const SCHEDULE_FILE_PATH = 'data/2019_2020_NHL_Schedule.csv';
+module.exports = {
+  SCORES_COLLECTION: 'scores-2019',
+  SCHEDULE_COLLECTION: 'schedule',
+  SCHEDULE_FILE_PATH: 'data/2019_2020_NHL_Schedule.csv',
+};
